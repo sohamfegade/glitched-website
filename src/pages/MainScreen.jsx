b8adba2039106740c851587428b415990e129b9f
Soham Fegade <sohamfegade3@gmail.com>
2026-03-26 22:36:56 +0530

    Updated Text in Mainscreen

--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -215,7 +215,7 @@ export default function MainScreen() {
                     textShadow: '0 0 10px rgba(0,247,255,0.5)',
                   }}
                 >
-                  Final Transmission Detected...
+                  Transmission Detected...
                 </motion.p>
               </div>
 
